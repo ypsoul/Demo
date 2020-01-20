c82dc305799f1ab90437faa05a4ae93a17cfcce3
印鹏 <ypsoul94@163.com>
2020-01-20 18:30:10 +0800

    yp

---
+++ nodejs-babel-es6/index.js
@@ -0,0 +1,3 @@
+import a,{b,c} from "./test"
+
+console.log(a,b,c)
---
+++ nodejs-babel-es6/test.js
@@ -0,0 +1,10 @@
+const a = 1;
+
+export default a; // 一个文件只能有一个默认导出 （export default）
+
+
+export const b = 2;
+
+export function c(){
+  console.log('按需方法')
+}
